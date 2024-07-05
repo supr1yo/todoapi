73ab451acc9e10a0f4c4af447d7ad13b917b3fa2
supr1yo <supriyo.developer@gmail.com>
2024-07-05 21:57:30 +0530

    Update ID as primary key

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,6 +3,11 @@ import sequelize from "../database/database";
 
 
 const User = sequelize.define('User', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+        autoIncrement: false,
+    },
     email: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,   
