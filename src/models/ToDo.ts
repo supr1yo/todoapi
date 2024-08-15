84930e3ccec227ce4172d73cecdd3ebd9fa63279
supr1yo <supriyo.developer@gmail.com>
2024-08-15 11:34:34 +0530

    ToDo Schema

--- src/models/ToDo.ts
+++ src/models/ToDo.ts
@@ -1,2 +1,38 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../database/database";
+
+const ToDo = sequelize.define('ToDo', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+        autoIncrement: false,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 100,
+            max: 250,
+        }
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 300,
+            max: 500,
+        }
+    },
+    isDone: {
+        type: DataTypes.BOOLEAN,
+    },
+    createdBy: {
+        type: DataTypes.STRING,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
+    },
+});
+
+export default ToDo;
